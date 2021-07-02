e0ee9c888172536d5559cf8af0fad50fcdb59b9d
Jeonghun Ha <maemenaver@soongsil.ac.kr>
2021-07-02 23:23:25 +0900

    ✨ Solve 11005 - 진법 변환 2

---
+++ 11005.py
@@ -0,0 +1,16 @@
+import string
+
+n, b = list(map(int, input().split()))
+
+tmp = string.digits+string.ascii_uppercase
+
+
+def convert(num, base):
+    q, r = divmod(num, base)
+    if q == 0:
+        return tmp[r]
+    else:
+        return convert(q, base) + tmp[r]
+
+
+print(convert(n, b))
